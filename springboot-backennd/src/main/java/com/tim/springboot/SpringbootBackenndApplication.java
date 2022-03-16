4ac48b645f1f7c9cf768ba0ce08e85a6fc07495c
tastytim <tastytimgm@gmail.com>
2022-03-16 18:28:24 +0100

    crud

---
+++ springboot-backennd/src/main/java/com/tim/springboot/SpringbootBackenndApplication.java
@@ -0,0 +1,13 @@
+package com.tim.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackenndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackenndApplication.class, args);
+	}
+
+}
---
+++ springboot-backennd/src/test/java/com/tim/springboot/SpringbootBackenndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackenndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
